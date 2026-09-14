3755cfeae8b0a8f87e1776d648c97545f021d3f7
4miruddin99-gif <4miruddin99@gmail.com>
2026-09-14 10:36:46 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,101 @@
+const CACHE_NAME = "amir-perodua-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./axia.png",
+  "./bezza.png",
+  "./myvi.png",
+  "./ativa.png",
+  "./alza.png",
+  "./aruz.png",
+  "./traz.png",
+  "./amir-perodua-header.png"
+];
+
+self.addEventListener("install", function(event) {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(ASSETS);
+      })
+  );
+
+  self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", function(event) {
+
+  event.waitUntil(
+
+    caches.keys().then(function(names) {
+
+      return Promise.all(
+
+        names.map(function(name) {
+
+          if(name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", function(event) {
+
+  if(event.request.mode === "navigate") {
+
+    event.respondWith(
+
+      fetch(event.request)
+        .then(function(response) {
+
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put("./index.html", copy);
+            });
+
+          return response;
+
+        })
+        .catch(function() {
+
+          return caches.match("./index.html");
+
+        })
+
+    );
+
+    return;
+
+  }
+
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(function(cached) {
+
+        return cached || fetch(event.request);
+
+      })
+
+  );
+
+});
